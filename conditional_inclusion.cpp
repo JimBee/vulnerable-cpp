00801b842f9c2431b0dbe524676783f0c63fe4ba
CI <-ci@not-real.com>
2024-09-30 17:58:32 +0100

    make conditionalFunc and vulnerableFunction do the same thing.

--- conditional_inclusion.cpp
+++ conditional_inclusion.cpp
@@ -3,11 +3,11 @@
 
 #ifdef FEATURE_ENABLED
 void conditionalFunc() {
-    std::cout << "Vulnerable feature is enabled!" << std::endl;
+    const char* largeInput = "This is a very large input that will overflow the buffer";
     char buffer[10];
-    std::cout << "Enter a string: ";
-    std::cin >> buffer; // This can cause buffer overflow if input exceeds 10 characters
-    std::cout << "You entered: " << buffer << std::endl;
+    // Vulnerable to buffer overflow
+    strcpy(buffer, largeInput);
+    std::cout << "Buffer content from conditionalFunc: " << buffer << std::endl;
 }
 #else
 void conditionalFunc() {
@@ -15,18 +15,16 @@ void conditionalFunc() {
 }
 #endif
 
-void vulnerableFunction(const char* input) {
+void vulnerableFunction() {
+    const char* largeInput = "This is a very large input that will overflow the buffer";
     char buffer[10];
     // Vulnerable to buffer overflow
-    strcpy(buffer, input);
-    std::cout << "Buffer content: " << buffer << std::endl;
+    strcpy(buffer, largeInput);
+    std::cout << "Buffer content from vulnerableFunction: " << buffer << std::endl;
 }
 
 int main() {
     conditionalFunc();
-
-    const char* largeInput = "This is a very large input that will overflow the buffer";
-    vulnerableFunction(largeInput);
-    
+    vulnerableFunction();
     return 0;
 }
